e0d89317fea48d783d6ac1ca6b535d4668d56e8e
wangkai <wangkai@wafersystems.com>
2020-04-15 00:07:57 +0800

    文章curd完成

--- routes/articles.js
+++ routes/articles.js
@@ -10,4 +10,28 @@ router.get('/', async function (req, res, next) {
   res.json({ data });
 });
 
+router.post('/', async function (req, res, next) {
+  const { body } = req;
+
+  await model.Article.create(body);
+  res.json({ result: 0 });
+});
+
+router.get('/:id', async function (req, res) {
+  const data = await model.Article.findByPk(req.params.id);
+  console.log(data);
+  res.json({ data });
+});
+
+router.put('/:id', async function (req, res) {
+  const data = await model.Article.findByPk(req.params.id);
+  data.update(req.body);
+  res.json({ result: 0 });
+});
+
+router.delete('/:id', async function (req, res) {
+  const data = await model.Article.findByPk(req.params.id);
+  data.destroy();
+  res.json({ result: 0 });
+});
 module.exports = router;
