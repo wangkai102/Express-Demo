4842bb1103665d71f911fa3dc7532ba59015b909
wangkai <wangkai@wafersystems.com>
2020-04-15 00:27:06 +0800

    完成模糊查询

--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const model = require('../models');
+const models = require('../models');
+const Op = models.Sequelize.Op;
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  const data = await model.Article.findAll({
+  const where = {};
+  const { title } = req.query;
+  if (title) {
+    where.title = {
+      [Op.like]: `%${title}%`,
+    };
+  }
+  const data = await models.Article.findAll({
     order: [['id', 'DESC']],
+    where,
   });
   res.json({ data });
 });
@@ -13,24 +22,24 @@ router.get('/', async function (req, res, next) {
 router.post('/', async function (req, res, next) {
   const { body } = req;
 
-  await model.Article.create(body);
+  await models.Article.create(body);
   res.json({ result: 0 });
 });
 
 router.get('/:id', async function (req, res) {
-  const data = await model.Article.findByPk(req.params.id);
+  const data = await models.Article.findByPk(req.params.id);
   console.log(data);
   res.json({ data });
 });
 
 router.put('/:id', async function (req, res) {
-  const data = await model.Article.findByPk(req.params.id);
+  const data = await models.Article.findByPk(req.params.id);
   data.update(req.body);
   res.json({ result: 0 });
 });
 
 router.delete('/:id', async function (req, res) {
-  const data = await model.Article.findByPk(req.params.id);
+  const data = await models.Article.findByPk(req.params.id);
   data.destroy();
   res.json({ result: 0 });
 });
